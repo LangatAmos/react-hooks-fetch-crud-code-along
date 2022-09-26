13459d62387652c1aff1076a688b977c7cb3fd2b
Amos Langat <amos.langat@student.moringaschool.com>
2022-09-26 18:39:10 +0300

    complete assignment

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,27 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({onAddItem}) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = {
+      name: name,
+      category: category,
+      isInCart: false
+    }
+    fetch(`http://localhost:4000/items`, {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then((newItemData) => onAddItem(newItemData))
+  }
+
   return (
-    <form className="NewItem">
+    <form onSubmit={handleSubmit} className="NewItem">
       <label>
         Name:
         <input
